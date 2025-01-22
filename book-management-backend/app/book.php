a7f39f7ebfc660cbc9bf5bc7e45cab9b9d40eb2f
Kalana Wasala <Wasalakalana@gmail.com>
2025-01-22 23:56:26 +0530

    create a model to table book

---
+++ book-management-backend/app/book.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class book extends Model
+{
+    //Craete a model assosiate with database table
+    protected $fillable = [
+    "name".
+    "author".
+    "edition".
+    "description".
+    "price"
+    ];
+}
